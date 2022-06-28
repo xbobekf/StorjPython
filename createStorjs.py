91901a6baa0d17ffde0fa1c2c5a4818bb959c7f6
filip.bobek13 <filip.bobek13@gmail.com>
2022-06-28 13:04:46 +0200

    almost done

--- createStorjs.py
+++ createStorjs.py
@@ -1,20 +1,24 @@
 import os
 
-iterations = 1
+iterations = 97
 currentDir = os.getcwd()
-runPortRangeStart = 1000
-monitorPortRangeStart = 1100
+runPortRangeStart = 1002
+monitorPortRangeStart = runPortRangeStart+100
+
+for i in range(iterations):
+    print(runPortRangeStart+i)
+    os.system(f'identity create storagenode{i} --concurrency 12')
 
 for i in range(iterations):
 
-    os.system(f'identity create storagenode{i}')
+    #os.system(f'identity create storagenode{i} --concurrency 12')
     #os.system(f"mkdir -p ~/.local/share/storj/identity/storagenode{i}/identity")
 
     identityToken = input(f'Inser identity Token for {i} node: ')
     os.system(f'identity authorize storagenode{i} {identityToken}')
-    print(identityToken)
-    
+        
     os.system(f'mkdir {currentDir}/{runPortRangeStart+i} && \
+        mkdir {currentDir}/{runPortRangeStart+i}/identity && \
         mv ~/.local/share/storj/identity/storagenode{i}/* {currentDir}/{runPortRangeStart+i}/identity && \
         mkdir {currentDir}/{runPortRangeStart+i}/scripts && \
         mkdir {currentDir}/{runPortRangeStart+i}/storage')
@@ -23,16 +27,16 @@ for i in range(iterations):
         --user $(id -u):$(id -g) \
         --mount type=bind,source="{currentDir}/{runPortRangeStart+i}/identity",destination=/app/identity \
         --mount type=bind,source="{currentDir}/{runPortRangeStart+i}/storage",destination=/app/config \
-        --name storagenode{i} storjlabs/storagenode:latest'
+        --name storagenode{runPortRangeStart+i} storjlabs/storagenode:latest'
     
     setupScriptFile = open(f"{currentDir}/{runPortRangeStart+i}/scripts/setupScript.sh", "w")  # write mode
     setupScriptFile.write(setupScript)
     setupScriptFile.close()
 
     runScript =  f'docker run -d --restart unless-stopped --stop-timeout 300 \
-        -p {runPortRangeStart+i}:{runPortRangeStart+i}/tcp \
-        -p {runPortRangeStart+i}:{runPortRangeStart+i}/udp \
-        -p 127.0.0.1:{monitorPortRangeStart+i}:{monitorPortRangeStart+i} \
+        -p {runPortRangeStart+i}:28967/tcp \
+        -p {runPortRangeStart+i}:28967/udp \
+        -p 127.0.0.1:{monitorPortRangeStart+i}:14002 \
         -e WALLET="0x7f532f04bf17bdefc4d1093ff5ae24efff39c6cf" \
         -e EMAIL="filip.bobek13@gmail.com" \
         -e ADDRESS="212.5.194.56:{runPortRangeStart+i}" \
@@ -40,15 +44,15 @@ for i in range(iterations):
         --user $(id -u):$(id -g) \
         --mount type=bind,source="{currentDir}/{runPortRangeStart+i}/identity",destination=/app/identity \
         --mount type=bind,source="{currentDir}/{runPortRangeStart+i}/storage",destination=/app/config \
-        --name storagenode{i} storjlabs/storagenode:latest'
+        --name storagenode{runPortRangeStart+i} storjlabs/storagenode:latest'
         
     runScriptFile = open(f"{currentDir}/{runPortRangeStart+i}/scripts/runScript.sh", "w")  # write mode
     runScriptFile.write(runScript)
     runScriptFile.close()
 
-    watchScript =  f'docker run -d --restart=always --name watchtower{i} \
+    watchScript =  f'docker run -d --restart=always --name watchtower{runPortRangeStart+i} \
         -v /var/run/docker.sock:/var/run/docker.sock storjlabs/watchtower \
-        storagenode{i} watchtower{i} --stop-timeout 300s'
+        storagenode{runPortRangeStart+i} watchtower{runPortRangeStart+i} --stop-timeout 300s'
     
     watchScriptFile = open(f"{currentDir}/{runPortRangeStart+i}/scripts/watchScript.sh", "w")  # write mode
     watchScriptFile.write(watchScript)
